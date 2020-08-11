16d62e3ad8fcab0d29f4c2ea379072e94293e4ec
Coby Forrester <cobyforrester@Cobys-MacBook-Pro.local>
2020-08-11 09:13:18 -0700

    index.html->backend

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 require("dotenv/config");
 
 const app = express();
@@ -21,7 +22,10 @@ const urlRoutes = require("./routes/urlshortening");
 const homeRoute = require("./routes/home");
 
 //MIDDLEWARE
-app.use("/shorten", urlRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/shorten", urlRoutes);
 app.use("/", homeRoute);
 
 //START SERVER
--- routes/urlshortening.js
+++ routes/urlshortening.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/rand", (req, res) => {
+  console.log(req.body.longurl);
   res.send("We are on randurl");
 });
 
