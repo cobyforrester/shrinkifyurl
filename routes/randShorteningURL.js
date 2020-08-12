ee2d1b51a0274224218f3c23979a92a95b0fe21a
Coby Forrester <cobyforrester@Cobys-MacBook-Pro.local>
2020-08-11 21:25:42 -0700

    randurl endpoint WIP

--- routes/randShorteningURL.js
+++ routes/randShorteningURL.js
@@ -4,9 +4,18 @@ const URLRand = require("../models/RandURL");
 const RandURL = require("../models/RandURL");
 
 router.post("/", async (req, res) => {
-  const elem = new RandURL({
+  try {
+    const longurl = await URLRand.find({ longurl: req.body.longurl });
+    if (longurl.length > 0) {
+      res.json(longurl[0]);
+      return;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  const elem = new URLRand({
     longurl: req.body.longurl,
-    shorturl: "abcdef",
+    shorturl: "11",
   });
   try {
     const savedURL = await elem.save();
