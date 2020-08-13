356a4603453521f90dc42d8fddb010172e245807
Coby Forrester <cobyforrester@Cobys-MacBook-Pro.local>
2020-08-12 20:39:22 -0700

    converting hash to b62

--- routes/randShorteningURL.js
+++ routes/randShorteningURL.js
@@ -16,7 +16,7 @@ router.post("/", async (req, res) => {
   }
   const elem = new RandURL({
     longurl: req.body.longurl,
-    shorturl: createShortenedURL(req.body.longurl, 5),
+    shorturl: createShortenedURL(req.body.longurl),
   });
   try {
     const savedURL = await elem.save();
@@ -26,17 +26,75 @@ router.post("/", async (req, res) => {
   }
 });
 
-const createShortenedURL = (longurl, n) => {
-  let hash = crypto.createHash("sha256").update(longurl).digest("Base64");
-  hash = hash.slice(0, hash.length - 1);
-  let shorturl = hash.slice(0, n);
-  let shorturlFnl = "";
-  for (let i = 0; i < shorturl.length; i++) {
-    if (shorturl[i] === "+") shorturlFnl += "-";
-    else if (shorturl[i] === "/") shorturlFnl += "_";
-    else shorturlFnl += shorturl[i];
-  }
-  return shorturlFnl;
+const createShortenedURL = (longurl) => {
+  let shorturl = createHash(longurl);
+  return shorturl;
 };
+
+const generateHashAsInt = (longurl) => {
+  //GENERATES HASH NUMBER AS INT
+  return parseInt(crypto.createHash("sha1").update(longurl).digest("hex"), 16);
+};
+
+const createHash = (longurl) => {
+  let hash = generateHashAsInt(longurl);
+  let mapOfChars = mapChars();
+  let n = 1;
+  let strFnl = "";
+
+  while (Math.pow(62, n) < hash) {
+    //calculates number of chars it will be
+    n += 1;
+  }
+  for (let i = 0; i < n; i++) {
+    let numPow = n - i - 1; //calculating left to right base 62 rep
+    let calcPow = Math.pow(62, numPow);
+    if (calcPow <= hash) {
+      let mult = 1;
+      while (mult * calcPow < hash) {
+        mult += 1;
+      }
+      mult -= 1;
+      hash -= mult * calcPow;
+      strFnl += mapOfChars[mult];
+    } else {
+      strFnl += mapOfChars[0];
+    }
+  }
+  return strFnl;
+};
+
+const mapChars = () => {
+  let total = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let mapOfChars = {};
+  for (let i = 0; i < total.length; i++) {
+    mapOfChars[i] = total[i];
+  }
+  return mapOfChars;
+};
+
+/*
+for i in range(n):
+p = n - i - 1
+if pow(62, p) <= varId:
+    mult = 1
+    while mult * pow(62, p) < varId:
+        mult += 1
+    mult -= 1
+    varId -= mult * pow(62, p)
+    strFnl += mapChars[mult]
+else:
+    strFnl += "0"
+return strFnl
+def mapCharsDic():
+lAlph = 'abcdefghijklmnopqrstuvwxyz'
+uAlph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+nums = '0123456789'
+total = nums+lAlph+uAlph
+mapChars = {}
+for i, val in enumerate(total):
+mapChars[i] = val
+return mapChars
+*/
 
 module.exports = router;
